7bc24d61364312a9afcc74132db2b36b6c830081
elrumordelaluz <elrumordelaluz@hotmail.com>
2013-12-16 20:53:22 +0100

    little changes

--- js/main.js
+++ js/main.js
@@ -13,20 +13,26 @@ function init(){
 	createList();
 	control();
 	createResult();
-		// Stop intro animations
+	createCookie("visitor", "secondtime", 20);
+	noIntro();
+	return false;
+}
+
+function noIntro(){
+	// Stop intro animations
 		document.getElementById('stp1').classList.remove('start');
 		document.getElementById('stp2').classList.remove('start');
 		document.getElementById('stp3').classList.remove('start');
 		document.getElementById('stp4').classList.remove('start');
 		document.getElementById('stp5').classList.remove('start');
 		document.getElementById('header').classList.remove('start');
+		document.getElementById('initials').classList.remove('start');
 		document.getElementById('ps').classList.remove('start');
 		document.getElementById('preview').classList.remove('start');
 		document.getElementById('cd').classList.remove('start');
 		document.getElementById('openpreview').classList.remove('start');
 		document.getElementById('openpresets').classList.remove('start');
 		document.getElementById('opencode').classList.remove('start');
-	return false;
 }
 
 function selectPreset(e){
@@ -170,3 +176,32 @@ document.onkeypress = function(evt) {
 	}
 }; //keypress
 control();
+
+
+var visited = getCookie("visitor");
+if(visited == "secondtime")
+	noIntro();
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
